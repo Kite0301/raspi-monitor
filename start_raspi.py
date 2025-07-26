2f283dbe1f598851b2d29a7d1d07921fd048a0c2
Kaito Suzuki <kaito.titech@gmail.com>
2025-07-26 12:45:34 +0900

    Change port to 5001

--- start_raspi.py
+++ start_raspi.py
@@ -8,4 +8,4 @@ sys.path.insert(0, '/usr/lib/python3/dist-packages')
 # Import and run the main application
 if __name__ == '__main__':
     from app import socketio, app
-    socketio.run(app, host='0.0.0.0', port=8000, debug=False)
+    socketio.run(app, host='0.0.0.0', port=5001, debug=False)
